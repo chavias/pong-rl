63be3963ca94b9af3d1ac5d127fa1ac51aecc5f6
= <matias.chavez@mail.ch>
2023-09-07 14:41:35 +0200

    added enviroment.py as game engine mimiking the interface of openAI gym

---
+++ DQN-algorithm.py
@@ -0,0 +1,262 @@
+
+import enviroment
+import math
+import random
+import matplotlib
+import matplotlib.pyplot as plt
+from collections import namedtuple, deque
+from itertools import count
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+
+
+# set up matplotlib
+is_ipython = 'inline' in matplotlib.get_backend()
+if is_ipython:
+    from IPython import display
+
+plt.ion()
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+env = enviroment.game()
+
+#######################################################################
+#                        Replay memory
+#######################################################################
+
+Transition = namedtuple('Transition',
+                        ('state', 'action', 'next_state', 'reward'))
+
+class ReplayMemory:
+
+    def __init__(self, capacity):
+        self.memory = deque([], maxlen=capacity)
+
+    def push(self, *args):
+        self.memory.append(Transition(*args))
+
+    def sample(self, batch_size):
+        return random.sample(self.memory, batch_size)
+
+    def __len__(self):
+        return len(self.memory)
+
+
+#######################################################################
+#                        Agents 
+#######################################################################
+
+class DQN(nn.Module):
+    def __init__(self, n_observations, n_actions):
+        super(DQN, self).__init__()
+        self.layer1 = nn.Linear(n_observations, 32)
+        self.layer2 = nn.Linear(32, 32)
+        self.layer3 = nn.Linear(32, n_actions)
+
+    # Called with either one element to determine next action, or a batch
+    # during optimization. Returns tensor([[left0exp,right0exp]...]).
+    def forward(self, x):
+        x = F.relu(self.layer1(x))
+        x = F.relu(self.layer2(x))
+        return self.layer3(x)
+
+class agent:
+    def __init__(self):
+        self.policy_net = DQN(n_observations, n_actions).to(device)
+        self.target_net = DQN(n_observations, n_actions).to(device)
+        self.target_net.load_state_dict(self.policy_net.state_dict())
+        self.optimizer = optim.AdamW(self.policy_net.parameters(),
+                                  lr=LR, amsgrad=True)
+
+
+#######################################################################
+#                        Initialization 
+#######################################################################
+
+BATCH_SIZE = 128
+GAMMA = 0.99
+EPS_START = 0.9
+EPS_END = 0.05
+EPS_DECAY = 1000
+TAU = 0.005
+LR = 1e-4
+
+n_actions = 3 # up, down, wait
+n_observations = 5 # ball position, ball velocity, paddel center position
+
+agent_left = agent()
+agent_right = agent()
+
+memory = ReplayMemory(10000)
+steps_done = 0
+
+def select_action(agent, state):
+    global steps_done
+    sample = random.random()
+    eps_threshold = EPS_END + (EPS_START - EPS_END) * \
+        math.exp(-1. * steps_done / EPS_DECAY)
+    steps_done += 1
+    if sample > eps_threshold:
+        with torch.no_grad():
+            # t.max(1) will return the largest column value of each row.
+            # second column on max result is index of where max element was
+            # found, so we pick action with the larger expected reward.
+            return agent.policy_net(state).max(1)[1].view(1, 1)
+    else:
+        return torch.tensor([[env.sample()]], device=device, dtype=torch.long)
+
+
+episode_durations = []
+
+
+def plot_durations(show_result=False):
+    plt.figure(1)
+    durations_t = torch.tensor(episode_durations, dtype=torch.float)
+    if show_result:
+        plt.title('Result')
+    else:
+        plt.clf()
+        plt.title('Training...')
+    plt.xlabel('Episode')
+    plt.ylabel('Duration')
+    plt.plot(durations_t.numpy())
+    # Take 100 episode averages and plot them too
+    if len(durations_t) >= 100:
+        means = durations_t.unfold(0, 100, 1).mean(1).view(-1)
+        means = torch.cat((torch.zeros(99), means))
+        plt.plot(means.numpy())
+
+    plt.pause(0.001)  # pause a bit so that plots are updated
+    if is_ipython:
+        if not show_result:
+            display.display(plt.gcf())
+            display.clear_output(wait=True)
+        else:
+            display.display(plt.gcf())
+
+
+def optimize_model(agent):
+    if len(memory) < BATCH_SIZE:
+        return
+    transitions = memory.sample(BATCH_SIZE)
+    # Transpose the batch (see https://stackoverflow.com/a/19343/3343043 for
+    # detailed explanation). This converts batch-array of Transitions
+    # to Transition of batch-arrays.
+    batch = Transition(*zip(*transitions))
+
+    # Compute a mask of non-final states and concatenate the batch elements
+    # (a final state would've been the one after which simulation ended)
+    non_final_mask = torch.tensor(tuple(map(lambda s: s is not None,
+                                          batch.next_state)), device=device, dtype=torch.bool)
+    non_final_next_states = torch.cat([s for s in batch.next_state
+                                                if s is not None])
+    state_batch = torch.cat(batch.state)
+    action_batch = torch.cat(batch.action)
+    reward_batch = torch.cat(batch.reward)
+
+    # Compute Q(s_t, a) - the model computes Q(s_t), then we select the
+    # columns of actions taken. These are the actions which would've been taken
+    # for each batch state according to policy_net
+    state_action_values = policy_net(state_batch).gather(1, action_batch)
+
+    # Compute V(s_{t+1}) for all next states.
+    # Expected values of actions for non_final_next_states are computed based
+    # on the "older" target_net; selecting their best reward with max(1)[0].
+    # This is merged based on the mask, such that we'll have either the expected
+    # state value or 0 in case the state was final.
+    next_state_values = torch.zeros(BATCH_SIZE, device=device)
+    with torch.no_grad():
+        next_state_values[non_final_mask] = target_net(non_final_next_states).max(1)[0]
+    # Compute the expected Q values
+    expected_state_action_values = (next_state_values * GAMMA) + reward_batch
+
+    # Compute Huber loss
+    criterion = nn.SmoothL1Loss()
+    loss = criterion(state_action_values, expected_state_action_values.unsqueeze(1))
+
+    # Optimize the model
+    optimizer.zero_grad()
+    loss.backward()
+    # In-place gradient clipping
+    torch.nn.utils.clip_grad_value_(policy_net.parameters(), 100)
+    optimizer.step()
+
+
+######################################################################
+#
+# Below, you can find the main training loop. At the beginning we reset
+# the ironment and obtain the initial ``state`` Tensor. Then, we sample
+# an action, execute it, observe the next state and the reward (always
+# 1), and optimize our model once. When the episode ends (our model
+# fails), we restart the loop.
+#
+# Below, `num_episodes` is set to 600 if a GPU is available, otherwise 50 
+# episodes are scheduled so training does not take too long. However, 50 
+# episodes is insufficient for to observe good performance on CartPole.
+# You should see the model constantly achieve 500 steps within 600 training 
+# episodes. Training RL agents can be a noisy process, so restarting training
+# can produce better results if convergence is not observed.
+#
+
+if torch.cuda.is_available():
+    num_episodes = 600
+else:
+    num_episodes = 50
+
+for i_episode in range(num_episodes):
+    # Initialize the ironment and get it's state
+    state = env.reset()
+    state = torch.tensor(state, dtype=torch.float32, device=device).unsqueeze(0)
+    for t in count():
+        action = select_action(state)
+        observation, reward, terminated, truncated, _ = env.step(action.item())
+        reward = torch.tensor([reward], device=device)
+        done = terminated or truncated
+
+        if terminated:
+            next_state = None
+        else:
+            next_state = torch.tensor(observation, dtype=torch.float32, device=device).unsqueeze(0)
+
+        # Store the transition in memory
+        memory.push(state, action, next_state, reward)
+
+        # Move to the next state
+        state = next_state
+
+        # Perform one step of the optimization (on the policy network)
+        optimize_model()
+
+        # Soft update of the target network's weights
+        # θ′ ← τ θ + (1 −τ )θ′
+        target_net_state_dict = target_net.state_dict()
+        policy_net_state_dict = policy_net.state_dict()
+        for key in policy_net_state_dict:
+            target_net_state_dict[key] = policy_net_state_dict[key]*TAU + target_net_state_dict[key]*(1-TAU)
+        target_net.load_state_dict(target_net_state_dict)
+
+        if done:
+            episode_durations.append(t + 1)
+            plot_durations()
+            break
+
+print('Complete')
+plot_durations(show_result=True)
+plt.ioff()
+plt.show()
+
+######################################################################
+# Here is the diagram that illustrates the overall resulting data flow.
+#
+# .. figure:: /_static/img/reinforcement_learning_diagram.jpg
+#
+# Actions are chosen either randomly or based on a policy, getting the next
+# step sample from the gym ironment. We record the results in the
+# replay memory and also run optimization step on every iteration.
+# Optimization picks a random batch from the replay memory to do training of the
+# new policy. The "older" target_net is also used in optimization to compute the
+# expected Q values. A soft update of its weights are performed at every step.
+#
---
+++ enviroment.py
@@ -0,0 +1,102 @@
+
+import pygame
+import random
+
+screen_width = 700
+screen_height = 500
+paddle_height = 50
+random.seed = 42
+
+
+class game:
+    def __init__(self):
+        self.paddle_left = Paddle(x=0,y=200)
+        self.paddle_right = Paddle(x=699,y=200)
+        self.ball = Ball(300,100,5,5)
+    
+    @staticmethod
+    def sample():
+        return random.choice([1,2,3])
+
+    def reset(self):
+        """ resets game to new state"""
+        self.paddel_left.y = 200
+        self.paddle_right.y = 200
+        self.ball.x = 300 + random.uniform(-15,15)
+        self.ball.y = 100 + random.uniform(-15,15)
+        self.ball.vx = 5*random.choice([-1,1])
+        self.ball.vy = 5*random.choice([-1,1])
+        return [self.ball.x, self.ball.y, self.ball.vx, self.ball.vy,
+                self.paddle_left.y, self.paddle_right.y] # not sure if this is right
+
+    def step(self, action_left,action_right):
+        """ step through the game """
+        # update the game 
+        self.ball.update()
+        self.paddle_left.update(action_left)
+        self.paddle_right.update(action_right)
+        # perform collision detection
+        terminated, reward1, reward2 = self.collision_detection()
+        # collect observations
+        observation = (self.ball.x, self.ball.y, self.ball.vx, self.ball.vy,
+                        self.paddle_left.y, self.paddle_right.y)
+        return observation, reward1, reward2, terminated
+
+    def collision_detection(self) -> tuple:
+        """ updates velocity of the ball """
+        reward1 = 0
+        reward2 = 0
+        terminated = False
+        if self.ball.x >= screen_width or self.ball.x <= 0:
+            self.ball.bounce_wall()
+        if self.ball.x <= 0:
+            if (self.ball.y >= self.paddle_left.y - paddle_height/2 and
+                self.ball.y <= self.paddle_left.y + paddle_height/2):
+                self.ball.bounce_paddel()
+                reward1 = 1
+            else: 
+                terminated = True
+        if self.ball.x <= screen_width:
+            if (self.ball.y >= self.paddle_right - paddle_height/2 and
+                self.ball.y <= self.paddle_right + paddle_height/2):
+                self.ball.bounce_paddel()
+                reward2 = 1  
+            else: 
+                terminated = True      
+        return terminated, reward1, reward2
+
+class Ball:  
+    def __init__(self,x,y,vx,vy):
+        self.x = x
+        self.y = y
+        self.vx = vx
+        self.vy = vy
+
+    def update(self):
+        # updates position
+        self.x += self.vx
+        self.y += self.vy
+
+    def bounce_wall(self):
+        # updates velocity
+        self.vy = -self.vy
+
+    def bounce_paddel(self):
+        # updates velocity
+        self.vx = -self.vy
+
+class Paddle:
+    def __init__(self,x,y):
+        self.x = x
+        self.y = y
+
+    def update(self,action,pixles=5):
+        # updates paddle positon depending on the action
+        if action == 1:
+            self.y += pixles
+        elif action == 2:
+            self.y -= pixles
+        if self.y - paddle_height/2 < 0:
+          self.y = 0
+        elif self.y + paddle_height/2 > screen_height:
+          self.y = screen_height
